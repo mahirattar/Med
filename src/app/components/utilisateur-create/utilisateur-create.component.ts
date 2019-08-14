2898f834bf0f26e6e058cd5c43cb3bdf6a6a4b67
mahir.attar <mahirattar90@gmail.com>
2019-08-14 15:53:47 +0200

    final

--- src/app/components/utilisateur-create/utilisateur-create.component.ts
+++ src/app/components/utilisateur-create/utilisateur-create.component.ts
@@ -63,4 +63,35 @@ export class UtilisateurCreateComponent implements OnInit {
     }
   }
 
+changes(): void {
+  this.calcBMI();
+  this.categorize();
+  console.log(this.utilisateurForm.value.name)
+}
+
+calcBMI(){
+  var imc = 0;
+  if (this.utilisateurForm.value.taille != ''){
+  imc = (this.utilisateurForm.value.poids/Math.pow(this.utilisateurForm.value.taille/100,2));};
+
+  return imc.toFixed(2);
+}
+
+categorize(){
+  var imc=(this.utilisateurForm.value.poids/Math.pow(this.utilisateurForm.value.taille/100,2));
+  var category = '?';
+  if (this.utilisateurForm.value.taille != ''){
+  if(imc<16){ category="Anorexie ou dénutrition";} 
+  else if(imc>16.5 && imc<18.5) {category="Maigreur";}
+  else if(imc>18.5 && imc<25) {category="Corpulence normale";}
+  else if(imc>25 && imc<30) {category="Surpoids";}
+  else if(imc>30 && imc<35) {category="Obésité modérée (Classe 1)";}
+  else if(imc>35 && imc<40) {category="Obésité élevé (Classe 2)";}
+  else if(imc>40) {category="Obésite morbide ou massive";};}
+ 
+ return category;
+ }
+    
+  
+
 }
